6fd0a02ff88f2bc891c86e0e474e09c6dc4a747a
Quoc-Khuong Tran <71856445+quockhuongtrann@users.noreply.github.com>
2022-05-25 18:10:23 +0700

    Delete test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-with open("/home/tqkhuong/Documents/THESIS/preprocessIWSLT15/data15/train.en-vi.en", "r", encoding="utf-8") as f:
-    en = f.readlines()
-with open("/home/tqkhuong/Documents/THESIS/preprocessIWSLT15/data15/train.en-vi.vi", "r", encoding="utf-8") as f:
-    en = f.readlines()
